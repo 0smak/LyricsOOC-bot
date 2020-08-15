79cd49af55bb03f18470dde203fc875de0d529bd
Óscar Muñoz <osmak@protonmail.com>
2020-08-15 20:48:56 +0200

    changed structure

---
+++ src/server.js
@@ -0,0 +1,77 @@
+'use strict';
+const express = require('express');
+const config = require('./config');
+const artists = require('./artists');
+const lyricsUtil = require('./utils/lyrics.util');
+const fs = require('fs'); 
+const Twit = require('twit');
+const app = express();
+
+app.listen(process.env.PORT || 8080);
+console.log('Server started')
+
+
+const twit = new Twit({
+    consumer_key: config.twitter_API.api_key,
+    consumer_secret: config.twitter_API.api_key_secret,
+    access_token: config.twitter_API.access_token,
+    access_token_secret: config.twitter_API.access_token_secret
+});
+
+setInterval(() => {
+  console.log('Posting random');
+  postRandom();
+}, 600000);
+
+const stream = twit.stream('statuses/filter', { track: '@LyricsOOCbot -RT -from:@LyricsOOCbot' })
+stream.on('tweet', function (tweet) {
+    console.log('Got tweet: ' + tweet.text)
+    if(tweet.user.id != config.twitter_API.userId) {
+        const text = tweet.text.replace(/@LyricsOOCbot/g, '');
+        createMedia(text, {tweetId: tweet.id_str, user: `@${tweet.user.screen_name}`});
+    }
+});
+
+
+const createMedia = async (text, reply = undefined) => {
+    console.log('[createMedia] ' + text)
+    const imageData = await lyricsUtil.createImage(text);
+    if (imageData != null) {
+        postMedia(imageData.filename, imageData.name, imageData.artists, reply);
+    }
+}
+
+
+const postRandom = async () => {
+    const imageData = await lyricsUtil.createImage(undefined, artists.getRandomArtist());
+    if (imageData != null) {
+        postMedia(imageData.filename, imageData.name, imageData.artists);
+    }
+}
+
+const delay = ms => new Promise(r => setTimeout(r, ms));
+
+
+const postMedia = (filename, name, artists, reply) => {
+    twit.postMediaChunked({ file_path: filename }, function (err, data, response) {
+        if (data) {
+            console.log('Media uploaded')
+        }
+        const mediaIdStr = data.media_id_string
+        const meta_params = { media_id: mediaIdStr }
+        twit.post('media/metadata/create', meta_params, function (err, data, response) {
+            fs.unlinkSync(filename);
+            if (!err) {
+                let params = { status: `${name} by ${artists}`, media_ids: [mediaIdStr]};
+                if (reply != undefined) {
+                    params = {...params,  in_reply_to_status_id: reply.tweetId};
+                    params.status = `${reply.user} ${params.status}`  
+                }
+                console.log(params)
+                twit.post('statuses/update', params, function (err, data, response) {
+                    console.log('tweet posted!');
+                })
+            }
+        })
+    })
+}
---
+++ src/utils/lyrics.util.js
@@ -0,0 +1,107 @@
+const puppeteer = require('puppeteer');
+const template = require("../templates/imageTemplate");
+const geniusService = require('../services/genius.service');
+
+const getValidLyric = async id => {
+  let lyrics = '';
+  let lyric = '';
+  let attempts = 0;
+  while (lyrics.length === 0 && attempts < 10) {
+    lyrics = await geniusService.getLyrics(id);
+    lyric = getLine(lyrics);
+    attempts++;
+  }
+  console.log(lyric);
+  if (attempts >= 10 && lyrics.length === 0) {
+    console.log('Error on getting lyrics attempts are more than 10');
+  }
+  return lyric;
+}
+
+const getLine = lyrics => {
+  const lyricsArray = lyrics.split(/\r\n|\r|\n/);
+  const validLyrics = [];
+  for (const lyric of lyricsArray) {
+    if (isValidLine(lyric.trim())) validLyrics.push(lyric);
+  }
+  const r = Math.floor(Math.random() * validLyrics.length);
+  return validLyrics[r];
+}
+
+const isValidLine = lyric => {
+  lyric = lyric.trim();
+  return !(
+    (lyric.charAt(0) === '[' && lyric.charAt(lyric.length - 1) === ']') ||
+    lyric === ''
+  );
+}
+
+const generateImage = async(img, name, artists, lyric) => {
+  const filename = `./img/${name} ${artists} ${new Date().getTime()}.png`.replace(/\s+/g, '-');
+  let buffer = await(async () => {
+    const browser = await puppeteer.launch({
+      'args' : [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
+    });
+    const page = await browser.newPage();
+    const html = template.html(img, name, artists, lyric);
+    const css = template.css(img);
+    await page.setContent(html);
+    await page.addStyleTag(css);
+    const image = await page.screenshot({ path: filename, fullPage: true });
+    await browser.close();
+    return image;
+  })();
+  return filename;
+}
+
+const getSongData = async (q) => {
+  const { data } = await geniusService.search(q);
+  const hits = data.response.hits;
+  let hit;
+  let found = false;
+  for (let i = 0; i < hits.length && !found; i++) {
+    found =
+      ['favorites', 'favoritos', 'ranking'].find((w) =>
+        hits[i].result.full_title.includes(w)
+      ) === undefined;
+
+    if (found) hit = hits[i];
+  }
+  if(found) {
+    const id = hit.result.id;
+    const name = hit.result.title;
+    let artists = hit.result.primary_artist.name;
+    const feat = hit.result.full_title.match(/\(\bFt\b\..*?\)/gi);
+    artists = feat ? artists + ' ' + feat[0] : artists;
+    const img = hit.result.header_image_url;
+    return { id, name, artists, img };
+  }
+}
+
+const getSongFromArtist = async (idArtist) => {
+  const { data } = await geniusService.getArtistSongs(idArtist);
+  const songs = data.response.songs;
+  const song = songs[Math.floor(Math.random() * songs.length)];
+  return { id: song.id, name: song.title, artists: song.primary_artist.name, img: song.header_image_url };
+}
+
+const createImage = async (q = undefined, idArtist = -1) => {
+  if (q || idArtist > -1) {
+    const { id, name, artists, img } = q ?
+      await getSongData(q).then(obj => obj)
+      : await getSongFromArtist(idArtist);
+
+    const lyric = await(getValidLyric(id))
+    let filename = await generateImage(img, name, artists, lyric)
+      .then(f => f);
+    return { filename, name, artists };
+  }
+  return null;
+}
+
+module.exports = {
+  createImage,
+}
