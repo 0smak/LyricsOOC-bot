b6f1cade7abd79eb50df33b01620f0fdf3a74043
0smak <osmak@protonmail.com>
2020-08-15 19:04:24 +0200

    setted port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 'use strict';
 const twitter = require('./helpers/twitter');
+const express = require('express');
+const app = express();
+app.listen(process.env.PORT || 8080);
 
 const start = (q = undefined) => {
   console.log('Server started')
