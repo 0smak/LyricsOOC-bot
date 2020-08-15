abd78733794f6dae9d3c3310ffbad648b230230d
0smak <osmak@protonmail.com>
2020-08-15 19:13:37 +0200

    changed stream

--- src/helpers/twitter.js
+++ src/helpers/twitter.js
@@ -5,33 +5,31 @@ const GeniusService = require('../services/genius.service');
 const fs = require('fs');
 const Twit = require('twit');
 
-let twit;
 
-const start = async () => {
-    twit = await new Twit({
-        consumer_key: config.twitter_API.api_key,
-        consumer_secret: config.twitter_API.api_key_secret,
-        access_token: config.twitter_API.access_token,
-        access_token_secret: config.twitter_API.access_token_secret
-    });
-    mentionsStream();
+const twit = await new Twit({
+    consumer_key: config.twitter_API.api_key,
+    consumer_secret: config.twitter_API.api_key_secret,
+    access_token: config.twitter_API.access_token,
+    access_token_secret: config.twitter_API.access_token_secret
+});
+
+
+const start = () => {
     setInterval(() => {
+        console.log('Posting random');
         postRandom();
     }, 1800000);
 }
 
-const mentionsStream = () => {
-    console.log('[mentionStream] started');
-    
-    const stream = twit.stream('statuses/filter', { track: ['@LyricsOOCbot -RT -from:@LyricsOOCbot'] })
-    stream.on('tweet', function (tweet) {
-        console.log('Got tweet: ' + tweet.text)
-        if(tweet.user.id != config.twitter_API.userId) {
-            const text = tweet.text.replace(/@LyricsOOCbot/g, '');
-            createMedia(text, {tweetId: tweet.id_str, user: `@${tweet.user.screen_name}`});
-        }
-    });
-}
+const stream = twit.stream('statuses/filter', { track: '@LyricsOOCbot -RT -from:@LyricsOOCbot' })
+stream.on('tweet', function (tweet) {
+    console.log('Got tweet: ' + tweet.text)
+    if(tweet.user.id != config.twitter_API.userId) {
+        const text = tweet.text.replace(/@LyricsOOCbot/g, '');
+        createMedia(text, {tweetId: tweet.id_str, user: `@${tweet.user.screen_name}`});
+    }
+});
+
 
 const createMedia = async (text, reply = undefined) => {
     console.log('[createMedia] ' + text)
